07727a5e35e21f75538a73bd52189fd372f6bb7b
Sufiyan don <sufiandar372@gmail.com>
2024-08-13 12:51:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /*
-WHATSAPP BOT BY Janith sadanuwan 
-WHATSAPP - 94767438882
-SUPPORT GROUP - https://t.me/QueenNilu
-YOUTUBE - http://youtube.com/janithsadanuwan
+WHATSAPP BOT BY Ansh King 
+WHATSAPP - 263710299896
+SUPPORT GROUP - https://t.me/Ansh_king_md_whatsapp
+YOUTUBE - http://youtube.com/MRANSHOFC
 */
 
 const fs = require('fs');
@@ -17,21 +17,21 @@ function convertToBool(text, fault = 'true') {
 global.SESSION_ID = '' // PUT your session ID BY scan QR 
 
 // databse URL  ##Required 
-global.POSTGRESQL_URL = ''
+global.POSTGRESQL_URL = 'mongodb+srv://anshking:anshking@cluster0.kiyxdxa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
  
 
 // ⚙️ OWNER SETTINGS  ⚙️
 
-global.OWNER_NUMBER = '94767438882'
+global.OWNER_NUMBER = '263773992855'
 
-global.OWNER_NAME = 'Janiya_X'
+global.OWNER_NAME = 'Ansh_X_King'
 
 global.TIMEZONE = 'Asia/Colombo' 
 
-global.INBOX_BLOCK_MESSAGE = '' // Inboc Block Message
+global.INBOX_BLOCK_MESSAGE = 'BLOCK !!!' // Inboc Block Message
 
-global.INBOX_BLOCK = 'false' // TO Inbox Block On put true
+global.INBOX_BLOCK = 'true' // TO Inbox Block On put true
 
 global.READ_MESSAGE = "true" 
 
@@ -41,19 +41,19 @@ global.OPENAI_API_KEY = "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"
 ⚙️ BOT  SETTINGS  ⚙️
 */
 
-global.BOT_NAME = 'ǫᴜᴇᴇɴ ɴɪʟᴜ'
+global.BOT_NAME = '𝙰𝙽𝚂𝙷 𝙺𝙸𝙽𝙶'
 
-global.FOOTER = 'Queen Nilu 2023'
+global.FOOTER = 'Ansh King 2024'
 
 global.BOT_OFFLINE = false    //  Always Onlne -  | false offline -True
 
-global.STICKER_NAME = '@Janiya 🤹‍♂️' //sticker
+global.STICKER_NAME = '@Ansh 🤹‍♂️' //sticker
 
 global.LANG = 'EN' // Language (EN-  TO english  ,  SI- TO sinhala )
 
 global.PREFIX = '.' // Bot Prefix
 
-global.CAPTION = 'ǫᴜᴇᴇɴ ɴɪʟᴜ  © 2023' // Caption
+global.CAPTION = '𝙰𝙽𝚂𝙷 𝙺𝙸𝙽𝙶  © 2024' // Caption
 
 global.ALIVELOGO = 'https://telegra.ph/file/1e63f0ee90304a12767c7.jpg' // Set Alive Logo link 
 
@@ -71,7 +71,7 @@ global.ANTI_BOTS = true
 
 global.ONLY_GROUP = 'false' // Only group mode
 
-global.GROUP_LINK = 'https://chat.whatsapp.com/IRdyJY5P4sF5BhtP5PIdHH' // ur group link
+global.GROUP_LINK = 'https://chat.whatsapp.com/IVvGoarnJdR5J97tPSMvK9' // ur group link
 
 
 /* 18 + Download Settings */
@@ -89,6 +89,6 @@ global.VOICE_REPLY = true
 
 /* V card */
 
-global.ytname = "YT: janithsadanuwan" //ur yt chanel name
-global.socialm = "GitHub: janithsadanuwan" //ur github or insta name
+global.ytname = "YT: mranshofc" //ur yt chanel name
+global.socialm = "GitHub: Don45678" //ur github or insta name
 global.location = "Srilanka, colombo" //ur location
